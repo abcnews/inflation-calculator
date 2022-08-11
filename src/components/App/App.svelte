385642420f64ac7aa7298ee5bb36c754a6df7d75
Julian Fell <hi@jtfell.com>
2022-08-11 15:59:36 +1000

    clean and comment code, add more csvs from ABS website in vain attempt at getting the magic 6.1%

--- src/components/App/App.svelte
+++ src/components/App/App.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
   import { setContext } from 'svelte';
   import { writable } from 'svelte/store';
+  import { Decimal } from 'decimal.js-light';
 
-  import Slider from '../Slider/Slider.svelte';
+  // import Slider from '../Slider/Slider.svelte';
   import { calculateInflationRate, getStoreData, InflationIndex } from '../../model';
 
-  let cpiIndex;
+  let cpiIndex: InflationIndex;
 
   // Create store with groups in it
   const indexStore = writable<InflationIndex>({});
   setContext('inflation-index', indexStore);
 
   getStoreData().then(d => {
-    cpiIndex = d;
-    indexStore.set(d)
+    cpiIndex = d.subsubgroup;
+    indexStore.set(d.subsubgroup)
   });
 
-  const formatPercentage = (x: number): string => `${(x * 100).toPrecision(2)}%`;
+  const formatPercentage = (x: Decimal): string => `${x.mul(100).toString()}%`;
+  // `${(x * 100).toPrecision(2)}%`;
 
 </script>
 
@@ -36,49 +38,37 @@
 
   <h2>Personal Inflation Calculator</h2>
 
-  <p>
-    Inflation has reached 6.1% in Australia. There’s no doubt that the cost of living is going up for everyone.
-    But the increase in prices isn't uniform, it’s dependent on what you buy.
-  </p>
-  <p>
-    Depending on the makeup of your "basket", the real inflation you're seeing could be quite different.
-
-    By reducing, or increasing, the <b>weighting</b> of each expenditure group in your budget, this page
-    will calculate your personal inflation rate.
-  </p>
-
-
-  <div class="sliders">
-    <h3>Transport</h3>
-    <p>
-      Transport saw big increases, but these were dominated by the price of new cars and petrol.
-    </p>
-    {#each Object.values($indexStore) as group}
-      {#if group.group === 'Transport'}
-        <Slider name={group.name} />
-      {/if}
-    {/each}
-
-    <h3>Housing</h3>
-    <p>
-      Housing construction costs rose sharply, but if you weren't buying a brand-new house or renovating, your inflation will be much lower.
-    </p>
-    {#each Object.values($indexStore) as group}
-      {#if group.group === 'Housing'}
-        <Slider name={group.name} />
-      {/if}
-    {/each}
-
-    <h3>Other Groups</h3>
-    <p>
-      The price increases across the other expenditure groups were modest.
-    </p>
-    {#each Object.values($indexStore) as group}
-      {#if group.group !== 'Transport' && group.group !== 'Housing'}
-        <Slider name={group.name} />
-      {/if}
-    {/each}
-  </div>
+  <!-- <div class="sliders"> -->
+  <!--   <h3>Transport</h3> -->
+  <!--   <p> -->
+  <!--     Transport saw big increases, but these were dominated by the price of new cars and petrol. -->
+  <!--   </p> -->
+  <!--   {#each Object.values($indexStore) as group} -->
+  <!--     {#if group.group === 'Transport'} -->
+  <!--       <Slider name={group.name} /> -->
+  <!--     {/if} -->
+  <!--   {/each} -->
+  <!--  -->
+  <!--   <h3>Housing</h3> -->
+  <!--   <p> -->
+  <!--     Housing construction costs rose sharply, but if you weren't buying a brand-new house or renovating, your inflation will be much lower. -->
+  <!--   </p> -->
+  <!--   {#each Object.values($indexStore) as group} -->
+  <!--     {#if group.group === 'Housing'} -->
+  <!--       <Slider name={group.name} /> -->
+  <!--     {/if} -->
+  <!--   {/each} -->
+  <!--  -->
+  <!--   <h3>Other Groups</h3> -->
+  <!--   <p> -->
+  <!--     The price increases across the other expenditure groups were modest. -->
+  <!--   </p> -->
+  <!--   {#each Object.values($indexStore) as group} -->
+  <!--     {#if group.group !== 'Transport' &#38;&#38; group.group !== 'Housing'} -->
+  <!--       <Slider name={group.name} /> -->
+  <!--     {/if} -->
+  <!--   {/each} -->
+  <!-- </div> -->
 </div>
 
 <style lang="scss">
@@ -89,9 +79,8 @@
   .inflation-calculator {
     font-family: "ABCSans",Helvetica,Arial,sans-serif;
 
-    p,
-    h2,
-    .sliders {
+    /* p, */
+    h2 {
       max-width: 600px;
       margin: auto;
       padding: 1rem;
--- src/model.ts
+++ src/model.ts
@@ -1,113 +1,175 @@
 import { csv } from 'd3-fetch';
+import { Decimal } from 'decimal.js-light';
 
+//
+// Model for representing an inflation index
+//
 export interface ExpenditureGroup {
   name: string;
   group: string;
-  annualInflation: number;
-  cpiWeighting: number;
-  // anuWeighting: number;
+  annualInflation: Decimal;
+  cpiWeighting: Decimal;
+  // anuWeighting: Decimal;
 
-  value: number; // defaults to cpiWeighting
+  value: Decimal; // defaults to cpiWeighting
 }
 
 export type InflationIndex = Record<string, ExpenditureGroup>;
 
-export function calculateInflationRate(index: InflationIndex): number {
-  return Object.values(index).reduce((acc, group) => {
-    // No idea if this is right
-    return acc + group.value * group.annualInflation;
-  }, 0);
+export interface InflationModel {
+  group: InflationIndex;
+  subgroup: InflationIndex;
+  subsubgroup: InflationIndex;
 }
 
-const GROUPS = [
-  { group: 'Housing', subgroup: 'Rents', label: 'Rents' },
-  { group: 'Housing', subgroup: 'New dwelling purchase by owner-occupiers', label: 'New houses' },
-  // { group: 'Housing', subgroup: 'Gas and other household fuels', label: 'Gas' },
-  { group: 'Housing', subgroup: 'other', label: 'Housing (other)' },
-  { group: 'Transport', subgroup: 'Motor vehicles', label: 'New cars' },
-  { group: 'Transport', subgroup: 'Automotive fuel', label: 'Petrol' },
-  { group: 'Transport', subgroup: 'other', label: 'Transport (other)' },
 
-  { group: 'Food and non-alcoholic beverages', label: 'Food' },
-  { group: 'Clothing and footwear', label: 'Clothing' },
-  { group: 'Health', label: 'Health' },
-  { group: 'Alcohol and tobacco', label: 'Alcohol and tobacco' },
-  { group: 'Furnishings, household equipment and services', label: 'Household items/services' },
-  { group: 'Communication', label: 'Communication' },
-  { group: 'Recreation and culture', label: 'Recreation and culture' },
-  { group: 'Education', label: 'Education' },
-  { group: 'Insurance and financial services', label: 'Insurance and financial services' },
-];
+// 
+// Weighted average
+// see: http://textbook.stpauls.br/Macroeconomics/page_90.htm
+//
+export function calculateInflationRate(index: InflationIndex): Decimal {
+  const weightedSum = Object.values(index).reduce((acc, group) => {
+    return acc.add(group.value.mul(group.annualInflation));
+  }, new Decimal(0));
 
-const calcSubgroupInflation = (latestYearSubgroups: any, name: string): number => {
-  const subgroupKey = `Percentage Change from Previous Period ;  ${name} ;  Australia ;`;
-  return latestYearSubgroups.reduce((acc, year) => acc + parseFloat(year[subgroupKey]), 0) / 100;
-};
-const calcSubgroupWeighting = (cpiWeights: any, name: string): number => {
-  return parseFloat(cpiWeights.find(w => w.Subgroup === name)['Subgroup weight']) / 100;
-};
-const calcGroupWeighting = (cpiWeights: any, name: string): number => {
-  return parseFloat(cpiWeights.find(w => w.Group === name && !w.Subgroup)['Group weight']) / 100;
+  const sumOfWeights = Object.values(index).reduce((acc, g) => acc.add(g.cpiWeighting), new Decimal(0));
+  return weightedSum.div(sumOfWeights);
+}
+
+const toPercentage = (x: string | number): Decimal => {
+  return new Decimal(x).div(100);
 };
 
-export async function getStoreData(): Promise<InflationIndex> {
+export async function getStoreData(): Promise<InflationModel> {
   const absolutePath = __webpack_public_path__ || '/';
-  const groupsData = await csv(`${absolutePath}cpi-groups.csv`);
-  const subgroupsData = await csv(`${absolutePath}cpi-subgroups.csv`);
-  const cpiWeights = await csv(`${absolutePath}cpi-weights.csv`);
-  const latestYearSubgroups = subgroupsData.slice(subgroupsData.length - 4);
 
-  const groups = GROUPS.map(group => {
+  // Downloaded from
+  //
+  // https://www.abs.gov.au/statistics/economy/price-indexes-and-inflation/consumer-price-index-australia/latest-release
+  //
+  const inflationData = await csv(`${absolutePath}annual-inflation.csv`);
+  const latestYearInflation = inflationData.find(a => a[""] === 'Jun-2022');
 
-    // Calc inflation for group or subgroup
-    let annualInflation = 0;
-    let cpiWeighting = 0;
-    if (group.subgroup === 'other') {
-      //
-      // Include the rest of the subgroups that haven't been used already to ensure we capture 100% of the basket
-      //
-      const excludedSubgroups = GROUPS.filter(g => g.group === group.group).map(g => g.subgroup);
-      const includedSubgroups = cpiWeights.filter(g =>
-        g.Group === group.group &&
-        !!g.Subgroup &&
-        excludedSubgroups.indexOf(g.Subgroup) === -1
-      );
+  // Downloaded from
+  //
+  // https://www.abs.gov.au/statistics/economy/price-indexes-and-inflation/annual-weight-update-cpi-and-living-cost-indexes/latest-release
+  //
+  // Appendix A
+  // Annual weight update of the CPI and Living Cost Indexes
+  // Reference period: 2021
+  //
+  const inflationWeights = await csv(`${absolutePath}inflation-weights.csv`);
 
-      cpiWeighting = includedSubgroups.reduce((acc, sg) => acc + parseFloat(sg['Subgroup weight']), 0) / 100;
+  // Downloaded from
+  //
+  // https://explore.data.abs.gov.au/vis?fs[0]=Economy%2C0%7CPrice%20indexes%20and%20inflation%23PRICE_INDEX_INFLATION%23&pg=0&fc=Economy&df[ds]=ECONOMY_TOPICS&df[id]=CPI&df[ag]=ABS&df[vs]=1.1.0&pd=2022-Q2%2C&dq=3.102675%2B102676%2B114121%2B114122%2B1144%2B115484%2B115485%2B115495%2B115496%2B115497%2B115498%2B115500%2B115501%2B115520%2B115524%2B115529%2B131178%2B131183%2B131185%2B131190%2B131192%2B131194%2B30014%2B40001%2B40002%2B40004%2B40005%2B40006%2B40007%2B40008%2B40009%2B40010%2B40012%2B40014%2B40015%2B40025%2B40026%2B40027%2B40029%2B40030%2B40034%2B40045%2B40046%2B40047%2B40048%2B40053%2B40055%2B40058%2B40060%2B40066%2B40067%2B40072%2B40073%2B40077%2B40078%2B40080%2B40081%2B40083%2B40084%2B40085%2B40086%2B40087%2B40088%2B40089%2B40090%2B40091%2B40092%2B40093%2B40094%2B40095%2B40096%2B40098%2B40101%2B40102%2B97549%2B97550%2B97551%2B97554%2B97555%2B97557%2B97558%2B97559%2B97560%2B97564%2B97567%2B97571%2B97572%2B97573%2B97574%2B20001%2B20002%2B20003%2B20004%2B20005%2B20006%2B115486%2B115488%2B115489%2B115493%2B126670%2B999901%2B999902%2B999903.20.50.Q&ly[rs]=INDEX&lb=nm
+  //
+  // Params:
+  // Measure: Percentage Change from Corresponding Quarter of the Previous Year
+  // Adjustment Type: Seasonally Adjusted
+  // Region: Weighted average of eight capital cities
+  // Frequency: Quarterly
+  // Time Period: 2022-Q2
+  // Unit of measure: Percent
+  //
+  // These numbers are _different_ to the ones on the website page, but both
+  const custom = await csv(`${absolutePath}custom.csv`);
 
-      //
-      // Weighted avaerage to combine the inflation of the remaining subgroups in the group
-      //
-      const combinedInflation = includedSubgroups.reduce((acc, sg) => {
-        // The amount of the remaining subgroups that this subgroup makes up
-        const weight = parseFloat(sg['Subgroup weight']) / 100;
-        const weightInIncludedGroups = weight / cpiWeighting;
-
-        // Weighted sum: inflation for subgroup & proportion of subgroup weighting
-        return acc + calcSubgroupInflation(latestYearSubgroups, sg.Subgroup) * weightInIncludedGroups;
-      }, 0);
-
-      // Find the weighted average by dividing by number of subgroups
-      annualInflation = combinedInflation / includedSubgroups.length;
-    } else if (group.subgroup) {
-      annualInflation = calcSubgroupInflation(latestYearSubgroups, group.subgroup);
-      cpiWeighting = calcSubgroupWeighting(cpiWeights, group.subgroup);
-    } else {
-      annualInflation = parseFloat(groupsData.find(g => g.Group === group.group).Annual) / 100;
-      cpiWeighting = calcGroupWeighting(cpiWeights, group.group);
-    }
-
+  // Clean the custom data explorer export to be usable in the later grouping
+  const inflationSeasonallyAdjusted = custom.map(i => {
+    const name = i['INDEX: Index'].split(': ')[1];
     return {
-      name: group.label,
-      group: group.group,
-      annualInflation,
-      cpiWeighting,
-      value: cpiWeighting,
+      name,
+      annualInflation: toPercentage(i.OBS_VALUE),
     };
   });
 
-  // console.log(groups);
-  // console.log(groups.reduce((acc, g) => acc + g.cpiWeighting, 0));
+  const keyPrefix = 'Percentage Change from Corresponding Quarter of Previous Year ;  ';
+  const keySuffix = ' ;  Australia ;';
 
-  return groups.reduce((acc, g) => ({ ...acc, [g.name]: g }), {});
+  const inflationByGroup: InflationModel = Object.keys(latestYearInflation).reduce((acc: InflationModel, key: string) => {
+    if (!key.startsWith(keyPrefix)) {
+      return acc;
+    }
+
+    // Clean the key so it's just the label of the group (this is shared across all the datasets)
+    const groupName = key.replace(keyPrefix, '').replace(keySuffix, '');
+    if (groupName === 'All groups CPI') {
+      return acc;
+    }
+
+    // Lookup the weightings using the key (groupName)
+    const group = inflationWeights.find(i => i.group === groupName)?.groupweight;
+    const subgroup = inflationWeights.find(i => i.subgroup === groupName)?.subgroupweight;
+    const subsubgroup = inflationWeights.find(i => i.subsubgroup === groupName)?.subsubgroupweight;
+
+    // each group will have exactly one of these values
+    const weighting: number = group || subgroup || subsubgroup;
+
+    // Leave out subgroup as it doesn't appear in the custom data
+    if (subgroup) {
+      return acc;
+    }
+
+    // Use the seasonally adjusted data
+    const { annualInflation } = inflationSeasonallyAdjusted.find(i => i.name === groupName);
+
+    // construct a generic group definition
+    const groupModel: ExpenditureGroup = {
+
+      // TRY BOTH THESE: Use one or the other of the inflation numbers per group
+      //    - seasonally adjusted data from custom explorer download
+      annualInflation: annualInflation, 
+      //    - regular download from ABS website
+      // annualInflation: toPercentage(latestYearInflation[key]),
+ 
+      name: groupName,
+      cpiWeighting: toPercentage(weighting),
+      value: toPercentage(weighting),
+      group: '', // TODO: figure out which top-level group to attach this to (doesn't matter at this stage)
+    };
+
+    // console.log(groupModel);
+
+    // Add it to the right level on the hierarchy
+    if (group) {
+      return {
+        ...acc,
+        group: {
+          ...acc.group,
+          [groupName]: groupModel,
+        },
+      };
+    }
+    if (subgroup) {
+      return {
+        ...acc,
+        subgroup: {
+          ...acc.subgroup,
+          [groupName]: groupModel,
+        },
+      };
+    }
+    if (subsubgroup) {
+      return {
+        ...acc,
+        subsubgroup: {
+          ...acc.subsubgroup,
+          [groupName]: groupModel,
+        },
+      };
+    }
+
+    throw new Error(`Not in any group type! ${groupName}`);
+  }, { group: {}, subgroup: {}, subsubgroup: {} } as InflationModel);
+
+  // Calculate the combined inflation rate using a weighted average at "group" level and "subsubgroup (or expenditure class)" level
+  console.log('inflation by group', calculateInflationRate(inflationByGroup.group).mul(100).toString());
+  console.log('inflation by subsubgroup', calculateInflationRate(inflationByGroup.subsubgroup).mul(100).toString());
+
+  // console.log(inflationByGroup);
+  // console.log('inflation by subgroup', calculateInflationRate(inflationByGroup.subgroup).mul(100).toString());
+
+
+  return inflationByGroup;
 }
