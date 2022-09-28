e4c888657240ddd69305f7ac4a192d93983f18ce
Julian Fell <hi@jtfell.com>
2022-09-28 16:39:58 +1000

    fix colours

--- src/colours.ts
+++ src/colours.ts
@@ -3,12 +3,12 @@ export const COLOURS = {
   'Alcohol and tobacco': '#00297E',
   Transport: '#A61268',
   Housing: '#664CB3',
-  Health: '#006B75',
   'Furnishings, household equipment and services': '#CF4452',
   'Education': '#6B003B',
   'Recreation and culture': 'deeppink',
-  Communication: '#00521C',
-  'Insurance and financial services': '#69788C',
+  Health: '#00521C',
+  Communication: '#69788C',
+  'Insurance and financial services': '#006B75',
   'Clothing and footwear': '#8FBF77',
 };
 
